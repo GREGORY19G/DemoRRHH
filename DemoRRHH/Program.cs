5e3d8fc79a0776113499f65ed2e93df47e5af0b4
gregory_dm <48815449+GREGORY19G@users.noreply.github.com>
2025-02-08 17:06:05 -0400

    Starting project
    Starting project with db connection

---
+++ DemoRRHH/Program.cs
@@ -0,0 +1,30 @@
+using DemoRRHH.Components;
+using DemoRRHH.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
